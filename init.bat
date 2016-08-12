df62a7957d1333dade892511228027156d7d7356
amanoese <amanoese@gmail.com>
2016-08-12 15:51:40 +0900

    initialize shell add

---
+++ init.bat
@@ -0,0 +1,6 @@
+mklink %HOME%\_vimrc %HOME%\.dotfiles\.vimrc
+mklink %HOME%\_gvimrc %HOME%\.dotfiles\.gvimrc
+mkdir %HOME%\.vim
+mkdir %HOME%\.vim\vimSwap
+mkdir %HOME%\.vim\vimBackUp
+mkdir %HOME%\.vim\undo
---
+++ init.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.gvimrc ~/.gvimrc
+mkdir ~/.vim
+mkdir ~/.vim/vimSwap
+mkdir ~/.vim/vimBackUp
+mkdir ~/.vim/undo
