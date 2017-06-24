ae717d2b6f090658f838dae5783ecc0550730ae6
amanoese <amanoese@gmail.com>
2017-06-24 05:05:25 +0000

    add sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+#/bin/sh
+ln -s ~/.dotfiles/.vimrc ~/.vimrc && \
+ln -s ~/.dotfiles/.gvimrc ~/.gvimrc && \
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig && \
 mkdir -p ~/.vim/vimSwap ~/.vim/vimBackUp ~/.vim/undo
