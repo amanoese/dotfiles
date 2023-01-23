50c7a5cfd912e443e7e113d8bcad7c44c28a2165
amanoese <amanoese@gmail.com>
2023-01-23 18:35:53 +0900

    fix support nvim & copilot

--- init.sh
+++ init.sh
@@ -7,6 +7,10 @@ ln -s ~/.dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 mkdir -p ~/.vim/vimSwap ~/.vim/vimBackUp ~/.vim/undo
 
+if [ -d ~/.config/nvim/ ]; then \
+  ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
+fi
+
 if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then \
   bash ./wsl_init.sh
 fi
